b9e56541167f76e29c42e4fa7420bf622959e95c
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-08-12 02:23:03 +0600

    added AppNavigator and screens

--- App.tsx
+++ App.tsx
@@ -1,46 +1,11 @@
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigator from './src/AppNavigator'
 
-
-function App(){
-
+const App = () => {
   return (
-    <SafeAreaView >
-      <View>
-        <Text>Hello world!</Text>
-        <Text>Hello world!</Text>
-        <Text>Hello world!</Text>
-      </View>
-    </SafeAreaView>
-  );
+    <AppNavigator/>
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
