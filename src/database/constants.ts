770f10ad8bb17b0deb396c6c22b5b02500ec1e5b
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-08-12 12:23:06 +0600

    feat: added database and installed dependencies

---
+++ src/database/constants.ts
@@ -0,0 +1,58 @@
+export const COLORS = {
+    white: '#ffffff',
+    black: '#000000',
+    green: '#00AC76',
+    blue: '#0043F9',
+    backgroundLight: 'F0F0F3',
+    backgroundMedium: '#B9B9B9',
+    backgroundDark: '#777777',
+}
+
+export const Items = [
+    {
+        id: 1,
+        productName: 'Sony Wireless Head...',
+        productPrice: 58.00,
+        description: 'bjkbjb',
+        rating: 4.5,
+        percentage: 88,
+        numberOfReviews: 420,
+        deliveryTime: '2-5 days',
+        image: 'https://www.gadstyle.com/wp-content/uploads/2020/12/sony-wh-ch510-wireless-headphones-1.jpg'
+    },
+    {
+        id: 2,
+        productName: 'Yoto Headphones',
+        productPrice: 44.99,
+        description: 'Listen to your favorite tracks wirelessly with a Bluetooth wireless technology by pairing your smartphone or tablet',
+        rating: 4.3,
+        percentage: 76,
+        numberOfReviews: 390,
+        deliveryTime: '2-5 days',
+        image: 'https://cdn.shopify.com/s/files/1/0310/7487/7577/products/PRACXX00423_headphones_Nocables_Noshadow_2000-removebg-preview_4f1f265e-6052-48b2-bbc4-0a0e70084887_1280x.webp?v=1676475948'
+    },
+    {
+        id: 3,
+        productName: 'Stylish Wire-free Blu...',
+        productPrice: 47.80,
+        description: 'Listen to your favorite tracks wirelessly with a Bluetooth wireless technology by pairing your smartphone or tablet',
+        rating: 4.3,
+        percentage: 76,
+        numberOfReviews: 390,
+        deliveryTime: '2-5 days',
+        image: 'https://d1gb7gicmr8iau.cloudfront.net/fit-in/1920x800/Media/Images/Product/Visual/23782_pictures_product_visual_1.png'
+    },
+    {
+        id: 4,
+        productName: 'Beats Studio Pro Wir...',
+        productPrice: 47.80,
+        description: 'Listen to your favorite tracks wirelessly with a Bluetooth wireless technology by pairing your smartphone or tablet',
+        rating: 4.3,
+        percentage: 76,
+        numberOfReviews: 390,
+        deliveryTime: '2-5 days',
+        image: 'https://www.araba.ae/cdn/shop/files/2_ae55ee80-e23a-43a0-9546-d0afa3f0ac52.webp?v=1707835755'
+    },
+
+
+]
