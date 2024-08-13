7b9988aa9dffd9b53df8c1c78025171c2b57c1d2
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-08-13 09:48:11 +0600

    feat: bottom nav modified

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import DetailsScreen from './screens/Details';
 import Favorites from './screens/Favorites';
 import Wallet from './screens/Wallet';
 import Profile from './screens/Profile';
+import { Image, Text, View } from 'react-native';
 
 export type StackParamList = {
   Search: undefined;
@@ -39,6 +40,7 @@ const App = () => {
     <NavigationContainer>
       <Tab.Navigator
       screenOptions={{
+        tabBarShowLabel: false,
         tabBarStyle: {
           position: 'absolute',
           elevation: 0,
@@ -47,11 +49,144 @@ const App = () => {
           paddingBottom: 20,
         }   
       }}>
-        <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-        <Tab.Screen name="Search" component={SearchStackNavigator} options={{ headerShown: false }} />
-        <Tab.Screen name="Favorites" component={Favorites} />
-        <Tab.Screen name="Wallet" component={Wallet} />
-        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Home" component={Home} 
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0 }}>
+              <Image
+                source={require('../src/database/icons/home.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#0043F9' : '#748c94',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#0043F9' : '#748c94',
+                  fontSize: 10,
+                  fontWeight: 800,
+                  marginTop: 6
+                }}
+              >
+                Home
+              </Text>
+            </View>
+          ),
+        }}
+      
+        />
+        <Tab.Screen name="Search" component={SearchStackNavigator} 
+         options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0 }}>
+              <Image
+                source={require('../src/database/icons/magnifying-glass.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#0043F9' : '#748c94',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#0043F9' : '#748c94',
+                  fontSize: 10,
+                  fontWeight: 800,
+                  marginTop: 6
+                }}
+              >
+                Search
+              </Text>
+            </View>
+          ),
+        }}
+       />
+        <Tab.Screen name="Favorites" component={Favorites} 
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0 }}>
+              <Image
+                source={require('../src/database/icons/heart.png')}
+                resizeMode="contain"
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: focused ? '#0043F9' : '#748c94',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#0043F9' : '#748c94',
+                  fontSize: 10,
+                  fontWeight: 800,
+                  marginTop: 6
+                }}
+              >
+               Favorites
+              </Text>
+            </View>
+          ),
+        }}
+      />
+        <Tab.Screen name="Wallet" component={Wallet}
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0 }}>
+              <Image
+                source={require('../src/database/icons/wallet.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#0043F9' : '#748c94',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#0043F9' : '#748c94',
+                  fontSize: 10,
+                  fontWeight: 800,
+                  marginTop: 6
+                }}
+              >
+                Wallet
+              </Text>
+            </View>
+          ),
+        }}
+       />
+        <Tab.Screen name="Profile" component={Profile}
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center', top: 0 }}>
+              <Image
+                source={require('../src/database/icons/person.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#0043F9' : '#748c94',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#0043F9' : '#748c94',
+                  fontSize: 10,
+                  fontWeight: 800,
+                  marginTop: 6
+                }}
+              >
+                Profile
+              </Text>
+            </View>
+          ),
+        }}
+       />
       </Tab.Navigator>
     </NavigationContainer>
   );
